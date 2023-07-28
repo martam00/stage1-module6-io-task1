2264fe9810a0ae35927154d2470a659e8a18c6ee
martam00 <marta.michniacka1@gmail.com>
2023-07-28 19:33:13 +0200

    file reader v2

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -11,16 +11,15 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
         Map<String, String> map = new HashMap<>();
-        try {
-            BufferedReader reader = new BufferedReader(new java.io.FileReader(file));
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] str = line.split(": ");
                 map.put(str[0], str[1]);
             }
-            reader.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
         String name = map.get("Name");
         Integer age = Integer.parseInt(map.get("Age"));
